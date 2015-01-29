2ef3cff7dda4b6c0b25ce6d513519bc445979184
Jim Forsythe <forsytjr@gmail.com>
2015-01-29 18:04:15 -0500

    lower pgain, raise igain.

--- src/RobotMap.cpp
+++ src/RobotMap.cpp
@@ -77,8 +77,8 @@ void RobotMap::init() {
 		// Add the Controllers
 			// Setup some parameters to be used by all controllers
 			// ToDo: These need to be calibrated
-			double pGain = 0.05;
-			double iGain = 0.0;
+			double pGain = 0.01;
+			double iGain = 0.01;
             double maxOutput = 1.0; //Power setting
             double maxRate = 15.0; // rev/sec
             double timeFilter = 0.01;
