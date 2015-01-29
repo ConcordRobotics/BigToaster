26b8df9d7aef56e6aed2089dfcabdf51177c2d93
Jim Forsythe <forsytjr@gmail.com>
2015-01-29 17:41:10 -0500

    Re-enable motor pGain

--- src/RobotMap.cpp
+++ src/RobotMap.cpp
@@ -76,7 +76,7 @@ void RobotMap::init() {
 		// Add the Controllers
 			// Setup some parameters to be used by all controllers
 			// ToDo: These need to be calibrated
-			double pGain = 0.0;
+			double pGain = 0.1;
 			double iGain = 0.0;
             double maxOutput = 1.0; //Power setting
             double maxRate = 15.0; // rev/sec
