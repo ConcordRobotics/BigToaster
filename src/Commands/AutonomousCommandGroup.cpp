02198ce80dbd879da1105d2abf8bc4e157b911bb
Jim Forsythe <forsytjr@gmail.com>
2015-03-21 15:19:03 -0400

    Changed distance for auto, added turns, increased gains.

--- src/Commands/AutonomousCommandGroup.cpp
+++ src/Commands/AutonomousCommandGroup.cpp
@@ -6,6 +6,7 @@
 #include "Commands/DriveToHeading.h"
 #include "Commands/ClawRate.h"
 #include "Commands/SetDriveMode.h"
+#include "Commands/SetGyroPower.h"
 
 #include "Robot.h"
 
@@ -14,7 +15,7 @@ AutonomousCommandGroup::AutonomousCommandGroup(int autoModeIn)
 	autoMode = autoModeIn;
 
 	if (autoMode == RobotMap::OFF) return;
-	AddSequential(new SetDriveMode(cPIDController::DIRECT));
+
 	double angle;
 	RobotMap::gyro->Reset();
 	//Robot::driveMotors->SetDriveMode(cPIDController::DIRECT);
@@ -22,21 +23,23 @@ AutonomousCommandGroup::AutonomousCommandGroup(int autoModeIn)
 	//AddSequential(new ClawGrip(0.01));
 	// Once it is gripped, continue to hold the grip
 	AddParallel(new ClawRate(-1.0));
+	AddSequential(new SetDriveMode(cPIDController::DIRECT));
+	AddSequential(new SetGyroPower(1.0));
 	// Lift the container
 	AddSequential( new LinearSysPosition(Robot::lift, Robot::lift, 0.30, 0.10));
 	//AddParallel(new LinearHoldPosition(Robot::lift, Robot::lift));
 	angle = 90.0;
 	AddSequential(new DriveToHeading(angle, 3.0));
-	AddSequential(new DriveForward(0.6, 5.0, 0.5, angle));
-    //if (autoMode == RobotMap::LEFT) {
-    //	angle = 180.0;
-    //	AddSequential(new DriveToHeading(angle, 3.0));
-    //} else if (autoMode == RobotMap::RIGHT) {
-    //	angle = 0.0;
-    //	AddSequential(new DriveToHeading(angle, 3.0));
-    //} else if (autoMode == RobotMap::CENTER) {
-    //	angle = 90.0;
-    //}
+	AddSequential(new DriveForward(0.6, 7.0, 0.5, angle));
+    if (autoMode == RobotMap::LEFT) {
+    	angle = 180.0;
+    	AddSequential(new DriveToHeading(angle, 3.0));
+    } else if (autoMode == RobotMap::RIGHT) {
+    	angle = 0.0;
+    	AddSequential(new DriveToHeading(angle, 3.0));
+    } else if (autoMode == RobotMap::CENTER) {
+    	angle = 90.0;
+    }
 	//AddSequential(new LinearSysPosition(Robot::lift, Robot::lift, 0.0, 0.15));
 	//AddParallel(new ClawRate(1.0));
 	//AddSequential(new DriveForward(0.6, -1.0, 0.1, angle));
